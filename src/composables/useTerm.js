b1e4cb0a2f62f815a830ea29776ca84f83f139a9
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-03 23:35:24 -0500

    Ajustando lógica

--- src/composables/useTerm.js
+++ src/composables/useTerm.js
@@ -1,37 +1,38 @@
-import DB from "@/data.json"
+import { HTTP } from "@/utils/fakeDB"
 
 export default () => {
   const GetAllTerms = async (options) => {
     const { limit = 10, page = 0 } = options
 
-    const items = DB.terms
-      .sort(function (a, b) {
-        if (a.slug > b.slug) {
-          return 1
-        }
-        if (a.slug < b.slug) {
-          return -1
-        }
-        return 0
-      })
-      .splice(page, limit)
-      .map(e => {
-        return {
-          id: e.id,
-          name: e.name,
-          slug: e.slug,
-        }
+    const items = await HTTP()
+      .then(async (response) => {
+        return response.terms
+          .sort(function (a, b) {
+            if (a.slug > b.slug) {
+              return 1
+            }
+            if (a.slug < b.slug) {
+              return -1
+            }
+            return 0
+          })
+          .splice(page, limit)
+          .map(e => {
+            return {
+              id: e.id,
+              name: e.name,
+              slug: e.slug,
+            }
+          })
       })
 
     return items
   }
 
   const GetOneTerm = async (id) => {
-    const item = await fetch(DB)
-      .then(res => res.json())
+    const item = await HTTP()
       .then(async (response) => {
-        const item = await response.terms.find(e => e.id === id)
-
+        const item = await response.terms.find((e) => e.id === id)
         return item
       })
 
