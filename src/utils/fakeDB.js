95a5ec775094d4de17135ea5cf799edee86d7f62
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-04 01:32:23 -0500

    Cambiando link

--- src/utils/fakeDB.js
+++ src/utils/fakeDB.js
@@ -1,5 +1,4 @@
 export const HTTP = async () => {
-  // return await fetch("https://ollorente.github.io/tourscorrecaminos/data.json")
-  return await fetch("http://localhost:5173/tourscorrecaminos/data.json")
+  return await fetch("https://ollorente.github.io/tourscorrecaminos/data.json")
     .then(res => res.json())
 }
